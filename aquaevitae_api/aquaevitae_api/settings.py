9c424a202ff08d9fb1846b2c2935860777b8e138
Guilherme <a57064@alunos.ipb.pt>
2024-04-18 19:07:55 +0100

    fix: Fix django settings

--- aquaevitae_api/aquaevitae_api/settings.py
+++ aquaevitae_api/aquaevitae_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", True)
+DEBUG = os.getenv("DEBUG", 0) == 1
 
 # Config Dev Dotenv
 from dotenv import load_dotenv
@@ -37,10 +37,12 @@ if DEBUG:
 else:
     CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(" ")
     CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(" ")
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SESSION_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(" ")
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS", 0) == 1
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Actual directory user files go to
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
